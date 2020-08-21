470cbc063ef83f3c94ade2b03ae40fc815b2ad39
Kavinrr <69468796+Kavinrr@users.noreply.github.com>
2020-08-21 19:29:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+//progress bar//
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+// click to scroll //
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+//refresh to top
+//window.onbeforeunload = function () 
+{
+ // window.scrollTo(0, 0);
+}
+
+//$(document).load().scrollTop(0);
+
+
+
